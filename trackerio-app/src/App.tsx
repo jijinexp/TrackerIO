d805ac33e85ad9797d7902ea8681e618d18cf4b4
jijinexp <jijinexp@gmail.com>
2023-08-11 17:24:05 +1200

    Adding default react files

---
+++ trackerio-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ListTransactions from "./Transactions/ListTransactions";
+
+function App() {
+  return (
+    <div className="App">
+       <ListTransactions />
+    </div>
+  );
+}
+
+export default App;
