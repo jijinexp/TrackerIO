8297f6ef4090387825df927c030bb5d0bf1b1de6
jijinexp <jijinexp@gmail.com>
2023-08-12 10:43:25 +1200

    Formatting table and adding Nav bar

--- trackerio-app/src/Transactions/ListTransactions.tsx
+++ trackerio-app/src/Transactions/ListTransactions.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 import '../App.css';
 import '../css/Table.css'
@@ -10,17 +10,19 @@ interface Transaction {
     description: string,
     type: string,
     bank: string,
-    amount: number
+    amount: number,
+    selected: boolean
 }
 
 function ListTransactions(){
     const itemsPerPage = 20; // Number of items per page
     const [currentPage, setCurrentPage] = useState(1);
-    const headers = ['Date', 'Description', 'Type','Bank','Amount'];
+    const [headers, setHeaders] = useState<string[]>([]);
     const [items, setItems] = useState<Transaction[]>([]);
+    const [data, setData] = useState<Transaction[]>([]);
 
     useEffect(() => {
-        fetchItems();
+        fetchItems().then();
     }, []);
 
     // Calculate the start and end indices of the current page's data
@@ -36,11 +38,29 @@ function ListTransactions(){
         try {
             await axios.get('http://localhost:5251/api/transactions').then(response =>
             {
-                setItems(response.data['rawTransactions']);
+                const {rawTransactions} = response.data;
+                setItems(rawTransactions);
+                if (rawTransactions && rawTransactions.length > 0) {
+                    // Filter out specific header names you don't need
+                    const excludedHeaders = ['id','csvId'];
+                    const filtered = Object.keys(rawTransactions[0])
+                        .filter(header => !excludedHeaders.includes(header));
+                    setHeaders(filtered);
+                }
             });
         }catch (error) {
             console.error('Error fetching items:', error)
         }
+    };
+
+    const handleCheckboxChange = (id: string) => {
+        // Update the state to reflect checkbox changes
+        setData(items => items.map(record => {
+            if (record.id === id) {
+                return { ...record, selected: !record.selected};
+            }
+            return record;
+        }));
     };
 
     return(
@@ -48,22 +68,27 @@ function ListTransactions(){
             <table>
                 <thead>
                 <tr>
+                    <th>Select</th>
                     {headers.map((header, index) => (
-                        <th key={index}>{header}</th>
+                        <th key={index}>{header.charAt(0).toUpperCase() + header.slice(1)}</th>
                     ))}
                 </tr>
                 </thead>
                 <tbody>
                 {displayedData.map((row, index) => (
-                    <tr key={index}>
-                        <td>{row.date}</td>
+                    <tr key={row.id}>
+                        <td>
+                            <input
+                                type="checkbox"
+                                checked={row.selected}
+                                onChange={() => handleCheckboxChange(row.id)}
+                            />
+                        </td>
+                        <td>{new Date(row.date).toLocaleDateString('en-NZ')}</td>
                         <td>{row.description}</td>
                         <td>{row.type}</td>
                         <td>{row.bank}</td>
-                        <td>{row.amount}</td>
-                        <td>
-                            <button>Edit</button>
-                        </td>
+                        <td className={row.amount > 0 ? 'positive' : 'negative' }>${row.amount}</td>
                     </tr>
                 ))}
                 </tbody>
